97154063ac6acce8479071139b6e376c55fc0f72
Yun Bei <yunbei@gmail.com>
2013-11-21 22:01:16 +0100

    Add table functionalities

--- CLA/src/main/java/nl/ing/cla/util/CLAUtil.java
+++ CLA/src/main/java/nl/ing/cla/util/CLAUtil.java
@@ -14,18 +14,23 @@ public class CLAUtil {
 	
 	private final static String DATA_LOCATION = "c:\\temp\\";
 	
-	private static String objectToJSONString(Object obj) throws JsonGenerationException, JsonMappingException, IOException {
+	private static String objectToJSON(Object obj) throws JsonGenerationException, JsonMappingException, IOException {
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		return ow.writeValueAsString(obj);	
 		
 	}
 	
-	public static void saveParent(String identifier, Object obj) throws JsonGenerationException, JsonMappingException, IOException {
-		final String fileName = DATA_LOCATION + identifier + ".txt";
+	
+	public static void saveFile(String identifier, Object obj) throws JsonGenerationException, JsonMappingException, IOException {
+		final String fileName = getFileName(identifier);
 		PrintWriter out = new PrintWriter(new File(fileName), "UTF-8");
-		out.write(objectToJSONString(obj));
+		out.write(objectToJSON(obj));
 		out.flush();
 		out.close();
 
+	}
+	
+	public static String getFileName(final String identifier) {
+		return  DATA_LOCATION + identifier + ".json";
 	}
 }
