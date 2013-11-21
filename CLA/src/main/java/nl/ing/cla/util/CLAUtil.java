c89ba2f0a49b6e476f01f96a2e48f858341557f1
Yun Bei <yunbei@gmail.com>
2013-11-21 21:48:28 +0100

    Add save to file util

--- CLA/src/main/java/nl/ing/cla/util/CLAUtil.java
+++ CLA/src/main/java/nl/ing/cla/util/CLAUtil.java
@@ -1,6 +1,9 @@
 package nl.ing.cla.util;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -8,9 +11,21 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 
 public class CLAUtil {	
-	public static String objectToJSONString(Object obj) throws JsonGenerationException, JsonMappingException, IOException {
+	
+	private final static String DATA_LOCATION = "c:\\temp\\";
+	
+	private static String objectToJSONString(Object obj) throws JsonGenerationException, JsonMappingException, IOException {
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		return ow.writeValueAsString(obj);	
 		
+	}
+	
+	public static void saveParent(String identifier, Object obj) throws JsonGenerationException, JsonMappingException, IOException {
+		final String fileName = DATA_LOCATION + identifier + ".txt";
+		PrintWriter out = new PrintWriter(new File(fileName), "UTF-8");
+		out.write(objectToJSONString(obj));
+		out.flush();
+		out.close();
+
 	}
 }
