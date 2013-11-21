fda884d2586f3a5b731857da3289b83b777e7cdc
Adriaan Thomas <athomas@xebia.com>
2013-11-22 00:53:26 +0100

    Data dir is now removed on shutdown.

--- CLA/src/main/java/nl/ing/cla/util/CLAUtil.java
+++ CLA/src/main/java/nl/ing/cla/util/CLAUtil.java
@@ -1,17 +1,19 @@
 package nl.ing.cla.util;
 
+import org.apache.commons.io.FileUtils;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.concurrent.atomic.AtomicLong;
-
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.ObjectWriter;
-import org.springframework.stereotype.Component;
 
 @Component
 public class CLAUtil implements Closeable {
@@ -48,6 +50,7 @@ public class CLAUtil implements Closeable {
 		try {
 			File file = Files.createTempDirectory("CLA").toFile();
 			file.deleteOnExit(); // a cowardly attempt, as there will probably be files in there
+			System.out.println("Using data directory: " + file.getAbsolutePath());
 			return file;
 		} catch (IOException e) {
 			// TODO handle this more properly
@@ -56,7 +59,9 @@ public class CLAUtil implements Closeable {
 	}
 
 	@Override
+	@PreDestroy
 	public void close() throws IOException {
+		System.out.println("Removing data directory: " + dataLocation.getAbsolutePath());
 		FileUtils.deleteDirectory(dataLocation);
 	}
 }
