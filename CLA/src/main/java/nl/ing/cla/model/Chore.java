ca9b2d38b8e548abce0637c3739599a4174ba146
unknown <YUNBEI@LBNL07892.corp.capgemini.com>
2013-11-21 20:47:26 +0100

    Chore data model

--- CLA/src/main/java/nl/ing/cla/model/Chore.java
+++ CLA/src/main/java/nl/ing/cla/model/Chore.java
@@ -1,5 +1,22 @@
 package nl.ing.cla.model;
 
 public class Chore {
+	int id;
+	String name;	
+	double price;
+	String status;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}	
 
 }
